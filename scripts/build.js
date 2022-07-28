23b8409b83e7a7c937a4c3c38872dde0f81ad0da
Muhamad Zolfaghari <muhamad.zolfaghari@gmail.com>
2022-07-28 09:41:52 +0430

    The definition was removed from project.

--- scripts/build.js
+++ scripts/build.js
@@ -4,33 +4,34 @@ const { execSync } = require("child_process");
 const path = require("path");
 const packageJson = require("../package.json");
 
-const DIST_DIR = "dist";
+const OUT_DIR = "dist";
 
 const renameLibFile = () => {
   const file = packageJson.name.split("/")[1] + ".js";
-  const filePath = path.resolve(DIST_DIR, file);
+  const filePath = path.resolve(OUT_DIR, file);
   fs.copySync(
-    path.resolve(DIST_DIR, filePath),
-    path.resolve(DIST_DIR, "index.js"),
+    path.resolve(OUT_DIR, filePath),
+    path.resolve(OUT_DIR, "index.js"),
     {}
   );
-  fs.removeSync(path.resolve(DIST_DIR, filePath));
+  fs.removeSync(path.resolve(OUT_DIR, filePath));
 };
 
 const copyFiles = (files) => {
   if (files) {
     files.forEach((file) => {
       const originPath = path.resolve(file);
-      const distPath = path.resolve(DIST_DIR, file);
+      const distPath = path.resolve(OUT_DIR, file);
       fs.copySync(originPath, distPath, {});
     });
   }
 };
 
 const runScript = (files) => {
+  fs.removeSync(OUT_DIR);
   execSync("npm run prebuild");
   renameLibFile();
   copyFiles(files);
 };
 
-runScript();
+runScript(["index.d.ts"]);
